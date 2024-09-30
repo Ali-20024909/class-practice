7ecfdc99c6d83aa15f82298d54901f124da9f313
Ali Hussnain <20042909@mydbs.ie>
2024-09-30 02:14:57 -0700

    loop

--- euler.js
+++ euler.js
@@ -1,24 +1,26 @@
 
 
  function sum(a,b,n)
- {
-    sum=0;
-    for(let i=1; i<n; i++){
-        if(i % a == 0 || i % b == 0){
-            sum +=i;
+    {
+        sum=0;
+        for(let i=1; i<n; i++)
+        {
+            if(i % a == 0 || i % b == 0)
+            
+                sum +=i;
+            
         }
-    }
         alert("Sum="+sum)
         
          
-        }
-         function GetValues()
-         {
+    }
+    function GetValues()
+    {
 
             num1= document.getElementById("num1").value;
             num2= document.getElementById("num2").value;
             limit= document.getElementById("limit").value;
             sum(num1,num2,limit);
 
-         }
+    }
  
