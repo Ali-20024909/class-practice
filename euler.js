a60ffe461e4c25eaf6e1a1d9c9c5f9456aedade9
Ali Hussnain <20042909@mydbs.ie>
2024-09-30 02:12:14 -0700

    n

--- euler.js
+++ euler.js
@@ -1,9 +1,9 @@
 
 
- function sum(a,b,b)
+ function sum(a,b,n)
  {
     sum=0;
-    for(let i=1; i<1000; i++){
+    for(let i=1; i<n; i++){
         if(i % a == 0 || i % b == 0){
             sum +=i;
         }
