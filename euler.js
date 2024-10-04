a48bf9e24f5a0bcfede762468138500d0412ac37
Ali Hussnain <20042909@mydbs.ie>
2024-10-04 01:07:56 -0700

    test.input

--- euler.js
+++ euler.js
@@ -20,7 +20,31 @@
             num1= document.getElementById("num1").value;
             num2= document.getElementById("num2").value;
             limit= document.getElementById("limit").value;
+            limit=document.getElementById("num1").style.display="none";
+            limit=document.getElementById("num2").style.display="none";
+            limit=document.getElementById("limit").style.display="none";
+            limit=document.getElementById("sum").style.display="none";
+            limit=document.getElementById("label1").style.display="none";
+            limit=document.getElementById("label2").style.display="none";
+            limit=document.getElementById("label3").style.display="none";
             sum(num1,num2,limit);
 
     }
+
+    function GetValues()
+    {
+
+            num1= document.getElementById("num1").style.display="block";
+            num2= document.getElementById("num2").style.display="block";
+            limit= document.getElementById("limit").style.display="block";
+            limit=document.getElementById("sum").style.display="block";
+            limit=document.getElementById("label1").style.display="block";
+            limit=document.getElementById("label2").style.display="block";
+            limit=document.getElementById("label3").style.display="block";
+            
+
+    }
+       
+   
+
  
